1fb10d5573231555781f38516154ccc54a8ac939
arbazXcode <jmi.arbaz1@gmail.com>
2026-01-18 18:35:09 +0530

    solve basic problems on codeforces

---
+++ 15_01_january/calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int M;
+
+    cin>>M;
+    cin>>N;
+
+    cout<<M<<" + "<<N<<" = "<<M+N<<endl;
+    cout<<M<<" - "<<N<<" = "<<M-N<<endl;
+    cout<<M<<" * "<<N<<" = "<<M*N<<endl;
+    cout<<M<<" / "<<N<<" = "<<M/N<<endl;
+    cout<<M<<" % "<<N<<" = "<<M%N<<endl;
+    return 0;
+}
