81a1068c1d7d1d109b7721ba3ef2f09d68df02a3
arbazXcode <jmi.arbaz1@gmail.com>
2026-01-18 18:47:50 +0530

    solve advance version of calculator i.e 10^9

---
+++ 15_01_january/calculatorII.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    long long M;
+    long long N;
+
+    cin>>M;
+    cin>>N;
+
+    cout<<M<<" + "<<N<<" = "<<M+N<<endl;
+    cout<<M<<" - "<<N<<" = "<<M-N<<endl;
+    cout<<M<<" * "<<N<<" = "<<M*N<<endl;
+    cout<<M<<" / "<<N<<" = "<<M/N<<endl;
+    cout<<M<<" % "<<N<<" = "<<M%N<<endl;
+    return 0;
+}
